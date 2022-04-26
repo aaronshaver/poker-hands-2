72949f11e62909db6f590b56ab40f5d9c54d9ee7
Aaron Shaver <aaronshaverpdx@gmail.com>
2022-04-26 15:42:28 -0700

    add support for number of hands as cmd line argument

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         // TODO: Accept cmd line arg: "generating X hands..." then show stats of each type and keys are ordered map?
-        System.out.println("Generating a poker hand...");
+        final Optional<String> numHandsRaw = Arrays.stream(args).findFirst();
+        int numHands;
+        if(numHandsRaw.isPresent()) {
+            numHands = Integer.parseInt(numHandsRaw.get());
+        } else {
+            throw new IllegalArgumentException("Must specify number of poker hands to generate as first argument");
+
+        }
+        System.out.printf("Generating %d poker hand(s)...%n", numHands);
     }
 
 }
