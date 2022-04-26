65f8f0f3dfb523de126c362c0c3b1bad044656bc
Aaron Shaver <aaronshaverpdx@gmail.com>
2022-04-26 09:57:02 -0700

    add todo re cmd line input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: Accept cmd line arg: "generating X hands..." then show stats of each type and keys are ordered map?
         System.out.println("Generating a poker hand...");
     }
 
